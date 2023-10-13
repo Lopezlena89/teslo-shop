2c3d8c226482e38364cc932267fe5f63197c8515
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2023-10-12 19:40:19 -0600

    Layouts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
 import type {NextPage} from 'next';
+import { ShopLayout } from '@/components/layouts';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import { initialData } from '@/database/products';
+
+initialData
 
 
 const Home:NextPage = () =>{
   return (
-    <h1>Teslo-shop</h1>
+    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos de Teslo aqui'}>
+        <Typography variant='h1' component='h1'>Tienda</Typography>
+        <Typography variant='h2' component='h2' sx={{mb:1}}>Todos los productos</Typography>
+
+
+        <Grid container spacing={4}>
+            {
+              initialData.products.map(product =>(
+                <Grid item xs={6} sm={4} key={product.slug}>
+                  <Card>
+                    <CardActionArea>
+                      <CardMedia
+                        component='img'
+                        image={`products/${product.images[0]}`}
+                        alt={product.title}
+                      />
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              ))
+            }
+        </Grid>
+
+
+    </ShopLayout>
   )
 }
 
