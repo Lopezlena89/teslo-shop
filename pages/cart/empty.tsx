21a06f14c169ed5bc5f1906839ec0144b4f06fb3
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2023-10-13 18:12:56 -0600

    cart shop

--- pages/cart/empty.tsx
+++ pages/cart/empty.tsx
@@ -1,9 +1,31 @@
+import NextLink from 'next/link';
+import { ShopLayout } from "@/components/layouts";
+import { RemoveShoppingCartOutlined } from "@mui/icons-material";
+import { Box, Link, Typography } from "@mui/material";
 
 
 
 const EmptyPage = () => {
   return (
-    <div>EmptyPage</div>
+    <ShopLayout title='Carrito vacio' pageDescription='No hay articulos en el carrito'>
+        <Box
+            display='flex' 
+            justifyContent='center' 
+            alignItems='center' 
+            height='calc(100vh - 200px)'
+            sx={{flexDirection:{xs:'column',sm:'row'}}}
+        >
+            <RemoveShoppingCartOutlined sx={{fontSize:100}}/>
+            <Box display='flex' flexDirection='column' alignItems='center'>
+                <Typography marginLeft={2}>Su carrito esta vacio</Typography>
+                <NextLink href='/' passHref legacyBehavior>
+                    <Link typography='h4' color='secondary'>
+                        Regresar
+                    </Link>
+                </NextLink>
+            </Box>
+        </Box>
+    </ShopLayout>
   )
 }
 
