adfbdf8b0cdec4701648028ef44069a3aefe8cac
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2023-10-13 19:50:14 -0600

    login/register

--- components/layouts/AuthLayout.tsx
+++ components/layouts/AuthLayout.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import Head from 'next/head'
+import { FC } from 'react'
 
-export const AuthLayout = () => {
+interface Props{
+    children:React.ReactNode;
+    title:string
+}
+
+export const AuthLayout:FC<Props> = ({children,title}) => {
   return (
-    <div>AuthLayout</div>
+    <>
+        <Head>
+            <title>{ title }</title>
+        </Head>
+
+        <main>
+            <Box display='flex' justifyContent='center' alignItems='center' height='calc(100vh - 200px)'>
+                {children}
+            </Box>
+        </main>
+    </>
+
   )
 }
